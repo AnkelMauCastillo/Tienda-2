b41066fbc745b1e9f4def0701cd14bf21e478dc2
AnkelMauCastillo <88917359+AnkelMauCastillo@users.noreply.github.com>
2021-10-27 21:47:25 -0500

    Actualización semana 25 al 29 de octubre 2021

--- src/main/java/mx/edu/uacm/progweb/tienda/dao/ProductoDAO.java
+++ src/main/java/mx/edu/uacm/progweb/tienda/dao/ProductoDAO.java
@@ -1,43 +1,109 @@
 package mx.edu.uacm.progweb.tienda.dao;
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 import mx.edu.uacm.progweb.tienda.conf.db.ConexionDB;
+import mx.edu.uacm.progweb.tienda.dominio.Producto;
 
 public class ProductoDAO {
 	
-	public boolean registrar(int clave, String nombre, Double precio, int cantidad) {
+	private Producto productoHallado;
+	private int clav,cant;
+	private String nom;
+	private Double precio;
+	private Connection conn = null;
+	//Creacion de consultas mediante cadenas
+	private Statement stm;
+	//Creacion de consultas parametrizadas
+	private PreparedStatement pstm;
+	private ResultSet productoresSet;
+
+	public boolean registrar(int clave, String nombre, Double precio, int cant) {
 		
-		Connection conn = null;
-		Statement stm;
+		System.out.println("> Entrando al metodo ProductoDAO.registrar <");
+				
 		int resultInsert = 0;
-		
-		
 		
 		try {
 			conn = ConexionDB.obtenerConexion();
 			stm = conn.createStatement();
 			
-			resultInsert = stm.executeUpdate("insert into producto values("
-			+ clave + ", '"
-			+ nombre + "', "
-			+ precio + ", "
-			+ cantidad + ");");
+			/*resultInsert = stm.executeUpdate("insert into producto values(" 
+					+ clave + ", '" 
+					+ nombre + "', " 
+					+ precio + ", " 
+					+ cant + ");");*/
+			
+			// --- Ejemplo de prepared stament ---
+			
+			String consulta = "insert into producto(clave, nombre, precio, cantidad) values(?, ?, ?, ?)";
+			pstm = conn.prepareStatement(consulta);
+			//binding (vinculación del valor y la posicion)
+			pstm.setInt(1, clave);
+			pstm.setString(2, nombre);
+			pstm.setDouble(3, precio);
+			pstm.setInt(4, cant);
+			
+			resultInsert = pstm.executeUpdate();
 			
 			
-			if (resultInsert != 0) {
+			if(resultInsert != 0) {
 				ConexionDB.cerrar();
 				return true;
 			} else {
 				ConexionDB.cerrar();
 				return false;
 			}
+				
+			
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			System.out.println("Error al insertar el producto en la BD: " + e);			
+			System.out.println("Error al insertar producto en bd: " + e);
+			
 			return false;
 		}
 		
+		
 	}
+	
+	public Producto consultar(int clave, String nombre) {
+		
+		System.out.println("> Entrando al metodo ProductoDAO.consultar <");
+		
+		
+		try {
+			conn = ConexionDB.obtenerConexion();
+			stm  = conn.createStatement();
+			
+			productoresSet = stm.executeQuery("select * from producto where clave='" + clave +"' and nombre='" + nombre.trim() + "'");
+			
+			if(!productoresSet.next()) {
+				System.out.println("No se encontro el registro");
+				ConexionDB.cerrar();
+				return null;
+			} else {
+				System.out.println("Se encontro el registro");
+				clav = productoresSet.getInt("clave");
+				nom = productoresSet.getString("nombre");
+				precio = productoresSet.getDouble("precio");
+				cant = productoresSet.getInt("cantidad");
+				
+				productoHallado = new Producto(clav, nom, precio, cant);
+				
+				ConexionDB.cerrar();
+				return productoHallado;
+				
+			}
+				
+			
+		} catch (SQLException e) {
+			System.out.println("Error al consultar el producto en bd: " + e);
+			return null;
+		}
+
+	}
+	
 }
--- src/test/java/mx/edu/uacm/progweb/tienda/dao/ProductoDAOTest.java
+++ src/test/java/mx/edu/uacm/progweb/tienda/dao/ProductoDAOTest.java
@@ -1,22 +1,48 @@
 package mx.edu.uacm.progweb.tienda.dao;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
+
+import mx.edu.uacm.progweb.tienda.dominio.Producto;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class ProductoDAOTest {
 	
 	@Test
 	public void debeRegistrarProducto() {
-		// TODO Auto-generated method stub
-		
-		System.out.println("> Entrando a debeRegistrarProducto");
+		System.out.println("> Entrando a debeRegistrarProducto <");
 		ProductoDAO dao = new ProductoDAO();
 		
-		boolean estaInsertadoRegistro = dao.registrar(45, "Red", Double.valueOf("55"), 8);
+		boolean estaInsertadoRegistro = dao.registrar(30, "Pesas", Double.valueOf("600"), 8);
 		
 		assertTrue(estaInsertadoRegistro);
-
+		
+	}
+	
+	@Test
+	public void debeObtenerProducto() {
+		System.out.println("> Entrando a debeObtenerProducto <");
+		ProductoDAO dao = new ProductoDAO();
+		
+		Producto productoHallado = dao.consultar(25, "Tapetes Yoga");
+		
+		//asercion me permite verificar que efectivamente obtuvo el objeto
+		assertNotNull(productoHallado);
+		
+	}
+	
+	@Test
+	public void noDebeObtenerProducto() {
+		System.out.println("> Entrando a noDebeObtenerProducto <");
+		ProductoDAO dao = new ProductoDAO();
+		
+		Producto productoHallado = dao.consultar(-100, "Tapetes Yoga");
+		
+		//asercion me permite verificar que efectivamente no se obtuvo el producto, es decir es nulo
+		assertNull(productoHallado);
+		
 	}
 
 }
